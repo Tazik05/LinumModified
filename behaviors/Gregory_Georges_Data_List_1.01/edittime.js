e8f8b5aa189d4ce2344eeb80b27b292c0f4c8ba7
Stazik <stasgamesstasinfo@gmail.com>
2023-02-28 00:35:06 +0300

    behaviors added

---
+++ behaviors/Gregory_Georges_Data_List_1.01/edittime.js
@@ -0,0 +1,109 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Datalist for Textbox",			// as appears in 'add behavior' dialog, can be changed as long as "id" stays the same
+		"id":			"datalist",			// this is used to identify this behavior and is saved to the project; never change it
+		"version":		"1.01",					// (float in x.y format) Behavior version - C2 shows compatibility warnings based on this
+		"description":	"Provide an auto-completion option to a textbox.",
+		"author":		"Gregory Georges",
+		"help url":		"http://goo.gl/gJgUI",
+		"category":		"Textbox",				// Prefer to re-use existing categories, but you can set anything here
+		"flags":		0						// uncomment lines to enable flags...
+						| bf_onlyone			// can only be added once to an object, e.g. solid
+	};
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])			// a number
+// AddStringParam(label, description [, initial_string = "\"\""])		// a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])			// accepts either a number or string
+// AddCmpParam(label, description)										// combo with equal, not equal, less, etc.
+// AddComboParamOption(text)											// (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])			// a dropdown list parameter
+// AddObjectParam(label, description)									// a button to click and pick an object type
+// AddLayerParam(label, description)									// accepts either a layer number or name (string)
+// AddLayoutParam(label, description)									// a dropdown list with all project layouts
+// AddKeybParam(label, description)										// a button to click and press a key (returns a VK)
+// AddAudioFileParam(label, description)								// a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+// Conditions
+
+// AddCondition(id,					// any positive integer to uniquely identify this condition
+//				flags,				// (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+//									// cf_deprecated, cf_incompatible_with_triggers, cf_looping
+//				list_name,			// appears in event wizard list
+//				category,			// category in event wizard list
+//				display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>, and {my} for the current behavior icon & name
+//				description,		// appears in event wizard dialog when selected
+//				script_name);		// corresponding runtime function name
+				
+// setWords
+AddStringParam("Datalist words", "Enter a list of words, separate by a comma ','.");
+AddAction(0, 0, "Set datalist words", "Datalist", "Set datalist words to {0}", "Set the datalist words.", "setWords");
+// addWord
+AddStringParam("Word", "Enter a word to add to the datalist");
+AddAction(1, 0, "Add a word to datalist", "Datalist", "Add {0} to datalist", "Add a word to the datalist words.", "addWord");
+
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+// new cr.Property(ept_integer,		name,	initial_value,	description)		// an integer value
+// new cr.Property(ept_float,		name,	initial_value,	description)		// a float value
+// new cr.Property(ept_text,		name,	initial_value,	description)		// a string
+// new cr.Property(ept_combo,		name,	"Item 1",		description, "Item 1|Item 2|Item 3")	// a dropdown list (initial_value is string of initially selected item)
+
+var property_list = [
+	new cr.Property(ept_text, 	"Datalist words",		"alpha,beta",		"Enter a list of words, separate by a comma ','")
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/Gregory_Georges_Data_List_1.01/runtime.js
@@ -0,0 +1,141 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+//           vvvvvvvvvv
+cr.behaviors.datalist = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+	//                               vvvvvvvvvv
+	var behaviorProto = cr.behaviors.datalist.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+		
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+	
+	behinstProto.onCreate = function()
+	{
+		/** Load properties **/
+		this.myProperty = this.properties[0];
+		
+		/** Apply behavior **/
+		var self = this;
+		this.wordsArray = [];
+		
+		/** on créer l'élément datalist **/
+		this.datalist = document.createElement("datalist");
+		this.datalist.id = "datalist"+this.inst.uid;
+		
+		/** on créer une array contenant la liste des mots **/
+		this.wordsArray  = this.properties[0].split(",");
+		var wordsArrayLength = this.wordsArray.length;
+		
+		for (var i = 0; i < wordsArrayLength; i++) 
+		{
+			var word = document.createElement("option");
+			word.value = self.wordsArray[i];
+			jQuery(word).appendTo(self.datalist);
+		}
+		
+		jQuery(document).ready( function() {
+			/** on récupère la textbox concernée **/
+			var textbox = self.inst.elem;
+			textbox.setAttribute("list", "datalist"+self.inst.uid);
+
+			/** on ajoute l'élément datalist au projet **/
+			jQuery(self.datalist).appendTo(self.runtime.canvasdiv ? self.runtime.canvasdiv : "body");
+		}); 
+
+	};
+	
+	behinstProto.tick = function ()
+	{
+		var dt = this.runtime.getDt(this.inst);
+		
+		// called every tick for you to update this.inst as necessary
+		// dt is the amount of time passed since the last tick, in case it's a movement
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	// the example action
+	Acts.prototype.setWords = function (wordslist)
+	{ 
+		if (wordslist != "") {
+			/** clear la datalist **/
+			this.wordsArray.length = 0;
+			this.datalist.innerHTML = "";
+			
+			this.wordsArray  = wordslist.split(",");
+			var wordsArrayLength = this.wordsArray.length;
+					
+			for (var i = 0; i < wordsArrayLength; i++) 
+			{
+				var word = document.createElement("option");
+				word.value = this.wordsArray[i];
+				jQuery(word).appendTo(this.datalist);
+			}
+		};		
+	};
+	
+	Acts.prototype.addWord = function (word)
+	{
+		if (word != "")
+		{
+			var _word = document.createElement("option");
+				_word.value = word;
+				jQuery(_word).appendTo(this.datalist);
+		};
+	};	
+	
+	behaviorProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+}());
